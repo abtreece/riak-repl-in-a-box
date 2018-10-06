516c97689df8e5c8318dd2f494ab4138590809fa
Britt Treece <britt.treece@gmail.com>
2018-10-06 10:40:46 -0500

    Overwrite riak.conf rather than appending

--- prestart.d/00-update-riak-conf.sh
+++ prestart.d/00-update-riak-conf.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Add standard config items
-cat <<END >>$RIAK_CONF
+cat <<END >$RIAK_CONF
 nodename = riak@$HOST
 distributed_cookie = $CLUSTER_NAME
 listener.protobuf.internal = 0.0.0.0:$PB_PORT
 listener.http.internal = 0.0.0.0:$HTTP_PORT
-riak.control = on
+riak_control = on
 END
 
 # Maybe add user config items
